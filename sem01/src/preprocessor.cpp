fee9d696cafebbcf2f89ae287662f2350f9f38eb
Maneren <maneren731@gmail.com>
2025-02-13 15:04:20 +0100

    sem01: refactor preprocessing

--- sem01/src/preprocessor.cpp
+++ sem01/src/preprocessor.cpp
@@ -15,30 +15,25 @@ bool valid_station(const Station &station) {
 }
 
 template <typename T>
-void swap_remove(std::vector<T> &vector, const size_t index) {
+inline void swap_remove(std::vector<T> &vector, const size_t index) {
   vector[index] = std::move(vector.back());
   vector.pop_back();
 }
 
 void SerialPreprocessor::preprocess_data(Stations &stations) const {
-  for (size_t i = 0; i < stations.size();) {
-    if (!valid_station(stations[i])) {
+  for (auto [i, station] :
+       stations | std::views::enumerate | std::views::reverse) {
+    if (!valid_station(station)) {
       swap_remove(stations, i);
-    } else {
-      ++i;
     }
   }
 }
 
 void ParallelPreprocessor::preprocess_data(Stations &stations) const {
-  auto futures = pool.transform(stations, valid_station);
-
-  auto i = 0;
-  for (auto &future : futures) {
+  for (auto [i, future] : pool.transform(stations, valid_station) |
+                              std::views::enumerate | std::views::reverse) {
     if (!future.get()) {
       swap_remove(stations, i);
-    } else {
-      ++i;
     }
   }
 }
