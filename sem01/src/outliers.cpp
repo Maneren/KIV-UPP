accc6416c6e49eb6851a61c5dd4ca16743789860
Maneren <maneren731@gmail.com>
2025-02-18 09:35:02 +0100

    sem01: cleanup

--- sem01/src/outliers.cpp
+++ sem01/src/outliers.cpp
@@ -5,7 +5,7 @@
 #include <utility>
 
 using StationMonthlyMinmaxes =
-    std::array<std::pair<Temperature, Temperature>, 12>;
+    std::array<std::ranges::minmax_result<Temperature>, 12>;
 
 std::pair<StationMonthlyAverages, StationMonthlyMinmaxes>
 calculate_monthly_stats(const Station &station) {
@@ -20,25 +20,23 @@ calculate_monthly_stats(const Station &station) {
   Temperature running_total{0};
 
   for (const auto &measurement : station.measurements) {
-    const auto year = measurement.year;
     const auto month = measurement.month;
-    const auto value = measurement.value;
 
     if (month != current_month) {
       const auto average = running_total / days;
 
       auto &current_min_max = monthly_minmaxes[month - 1];
-      current_min_max.first = std::min(current_min_max.first, average);
-      current_min_max.second = std::max(current_min_max.second, average);
+      current_min_max.min = std::min(current_min_max.min, average);
+      current_min_max.max = std::max(current_min_max.max, average);
 
-      monthly_averages[month - 1].push_back({year, average});
+      monthly_averages[month - 1].push_back({measurement.year, average});
 
       current_month = month;
       days = 0;
       running_total = 0;
     }
 
-    running_total += value;
+    running_total += measurement.value;
     days++;
   }
 
@@ -53,7 +51,7 @@ StationMonthlyAverages calculate_monthly_averages(const Station &station,
        std::views::zip(averages, min_maxes) | std::views::enumerate) {
     const auto [month, min_max] = item;
 
-    const auto allowed_difference = 0.75f * (min_max.second - min_max.first);
+    const auto allowed_difference = 0.75f * (min_max.max - min_max.min);
 
     for (const auto [prev, next] : std::views::pairwise(month)) {
       if (next.first != prev.first + 1) {
@@ -87,18 +85,18 @@ SerialOutlierDetector::find_averages_and_outliers(
 std::pair<std::vector<StationMonthlyAverages>, Outliers>
 ParallelOutlierDetector::find_averages_and_outliers(
     const Stations &stations) const {
-  auto futures = pool.transform(stations, [](const auto &station) {
+  auto process_station = [](const auto &station) {
     Outliers outliers;
     const auto monthly_averages = calculate_monthly_averages(station, outliers);
     return std::make_pair(monthly_averages, outliers);
-  });
+  };
 
   std::vector<StationMonthlyAverages> stations_averages;
   stations_averages.reserve(stations.size());
 
   Outliers outliers;
 
-  for (auto &future : futures) {
+  for (auto &future : pool.transform(stations, process_station)) {
     const auto &[averages, station_outliers] = future.get();
 
     stations_averages.push_back(std::move(averages));
