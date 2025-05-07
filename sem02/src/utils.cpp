e1abf3cda4910310dc221fdab93406f27e296f96
Maneren <maneren731@gmail.com>
2025-05-07 13:17:38 +0200

    sem02: remove unused

--- sem02/src/utils.cpp
+++ sem02/src/utils.cpp
@@ -109,13 +109,6 @@ std::string safeURL(const std::string &url) {
   const auto parsed = parseURL(url);
   std::string result = parsed.domain + parsed.path.string();
 
-  const static std::unordered_set<char> WHITELIST{
-      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-      'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-      'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-      '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'};
-
   // Use a lambda to check if a character is alphanumeric or in the allowed set
   const auto isAllowed = [](char c) {
     return std::isalnum(c) || c == '-' || c == '_';
