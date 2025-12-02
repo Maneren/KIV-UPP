7424524370c05c72c0575ceaf3646c94bb62ed8d
Maneren <maneren731@gmail.com>
2025-12-02 15:22:22 +0100

    sem01: implement parallel file parsing

--- sem01/src/main.cpp
+++ sem01/src/main.cpp
@@ -11,6 +11,7 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <mutex>
 #include <ostream>
 #include <ranges>
 #include <sstream>
@@ -125,8 +126,114 @@ Stations read_stations(const std::filesystem::path &input_filepath) {
   return stations;
 }
 
+std::tuple<size_t, Measurement> parse_line(const std::string_view line) {
+  auto tokens = std::views::split(line, ';');
+  auto iterator = tokens.begin();
+
+  auto id_str = iterator++;
+  if (id_str == tokens.end()) {
+    throw std::runtime_error("Failed to read id field.");
+  }
+  size_t id = str_to_number(*id_str);
+
+  auto ordinal_str = iterator++;
+  if (ordinal_str == tokens.end()) {
+    throw std::runtime_error("Failed to read ordinal field.");
+  }
+  size_t ordinal = str_to_number(*ordinal_str);
+
+  auto year_str = iterator++;
+  if (year_str == tokens.end()) {
+    throw std::runtime_error("Failed to read year field.");
+  }
+  Year year = str_to_number(*year_str);
+
+  auto month_str = iterator++;
+  if (month_str == tokens.end()) {
+    throw std::runtime_error("Failed to read month field.");
+  }
+  Month month = str_to_number(*month_str);
+
+  auto day_str = iterator++;
+  if (day_str == tokens.end()) {
+    throw std::runtime_error("Failed to read day field.");
+  }
+  Day day = str_to_number(*day_str);
+
+  auto value_str = iterator++;
+  if (value_str == tokens.end()) {
+    throw std::runtime_error("Failed to read value field.");
+  }
+  Temperature value = str_to_double(*value_str);
+
+  return {id, {ordinal, year, month, day, value}};
+}
+
+void process_measurements_serial(Stations &stations, std::string_view content) {
+  for (const auto line : std::views::split(content, '\n')) {
+    if (!line.empty()) {
+      const auto [id, measurement] = parse_line(std::string_view(line));
+      stations[id - 1].measurements.emplace_back(std::move(measurement));
+    }
+  }
+}
+
+void process_measurements_parallel(Stations &stations,
+                                   std::string_view content) {
+  // split the range into 2 MiB parts
+  constexpr size_t N = 1024 * 1024 * 2;
+
+  std::vector<std::mutex> mutexes(stations.size());
+
+  // parse each chunk in a separate thread
+  const auto process_chunk = [&content, &stations, &mutexes](const size_t i) {
+    auto chunk = content.substr(i);
+
+    if (i > 0) {
+      size_t newline_index = chunk.find('\n');
+      chunk = chunk.substr(newline_index + 1);
+    }
+
+    if (chunk.size() >= N) {
+      size_t newline_index = chunk.substr(N).find('\n');
+      if (newline_index != std::string::npos) {
+        chunk = chunk.substr(0, N + newline_index);
+      }
+    }
+
+    size_t last_id = std::numeric_limits<size_t>::max();
+
+    do {
+      size_t newline_index = std::min(chunk.find('\n'), chunk.size() - 1);
+      auto [id, measurement] = parse_line(chunk.substr(0, newline_index));
+      chunk = chunk.substr(newline_index + 1);
+
+      // hold the lock for the current station until we hit another one
+      // This exploits the fact that the data is sorted by station id
+      if (last_id != id) {
+        if (last_id != std::numeric_limits<size_t>::max()) {
+          mutexes[last_id - 1].unlock();
+        }
+        mutexes[id - 1].lock();
+        last_id = id;
+      }
+
+      stations[id - 1].measurements.push_back(measurement);
+    } while (!chunk.empty());
+
+    if (last_id != std::numeric_limits<size_t>::max()) {
+      mutexes[last_id - 1].unlock();
+    }
+  };
+
+  auto offsets = std::views::iota(0uz, content.size()) | std::views::stride(N);
+  threadpool::pool.for_each(std::move(offsets), process_chunk);
+}
+
 void fill_measurements(Stations &stations,
-                       const std::filesystem::path &input_filepath) {
+                       const std::filesystem::path &input_filepath,
+                       bool parallel) {
+
   auto size = std::filesystem::file_size(input_filepath);
   std::string file_string(size, '\0');
 
@@ -137,53 +244,20 @@ void fill_measurements(Stations &stations,
 
   file.read(file_string.data(), size);
 
-  for (const auto &line :
-       std::views::split(file_string, '\n') | std::views::drop(1)) {
-    if (line.empty()) {
-      continue;
-    }
+  if (file.fail()) {
+    throw std::runtime_error("Failed to read file.");
+  }
 
-    auto tokens = std::views::split(line, ';');
-    auto iterator = tokens.begin();
+  // skip header
+  size_t newline_index = file_string.find('\n');
 
-    auto id_str = iterator++;
-    if (id_str == tokens.end()) {
-      throw std::runtime_error("Failed to read id field.");
-    }
-    size_t id = str_to_number(*id_str);
+  std::string_view file_string_view =
+      std::string_view(file_string).substr(newline_index + 1);
 
-    auto ordinal_str = iterator++;
-    if (ordinal_str == tokens.end()) {
-      throw std::runtime_error("Failed to read ordinal field.");
-    }
-    size_t ordinal = str_to_number(*ordinal_str);
-
-    auto year_str = iterator++;
-    if (year_str == tokens.end()) {
-      throw std::runtime_error("Failed to read year field.");
-    }
-    Year year = str_to_number(*year_str);
-
-    auto month_str = iterator++;
-    if (month_str == tokens.end()) {
-      throw std::runtime_error("Failed to read month field.");
-    }
-    Month month = str_to_number(*month_str);
-
-    auto day_str = iterator++;
-    if (day_str == tokens.end()) {
-      throw std::runtime_error("Failed to read day field.");
-    }
-    Day day = str_to_number(*day_str);
-
-    auto value_str = iterator++;
-    if (value_str == tokens.end()) {
-      throw std::runtime_error("Failed to read value field.");
-    }
-    Temperature value = str_to_double(*value_str);
-
-    stations[id - 1].measurements.emplace_back(ordinal, year, month, day,
-                                               value);
+  if (parallel) {
+    process_measurements_parallel(stations, file_string_view);
+  } else {
+    process_measurements_serial(stations, file_string_view);
   }
 }
 
@@ -212,7 +286,8 @@ int main(int argc, char *argv[]) {
 
   auto stations = read_stations(config.stations_file());
 
-  fill_measurements(stations, config.measurements_file());
+  fill_measurements(stations, config.measurements_file(),
+                    config.mode() == Parallel);
 
   const auto elapsed = std::chrono::high_resolution_clock::now() - start;
 
